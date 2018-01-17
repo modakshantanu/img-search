6a5c3128daf769cf4ba1cad34d4fe9be6b40f430
Glitch (img-serach) <none>
2018-01-17 04:30:06 +0000

    🛀👕 Updated with Glitch

--- server.js
+++ server.js
@@ -4,34 +4,106 @@
 // init project
 var express = require('express');
 var app = express();
+var mongodb = require("mongodb");
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+'use strict';
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+let https = require('https');
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+// Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = process.env.KEY;
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+// Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+// search APIs.  In the future, regional endpoints may be available.  If you
+// encounter unexpected authorization errors, double-check this host against
+// the endpoint for your Bing Web search instance in your Azure dashboard.
+let host = 'api.cognitive.microsoft.com';
+let path = '/bing/v7.0/images/search';
+var k;
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+
+let response_handler = function (response) {
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        body = JSON.parse(body);
+        k = body.value;
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+};
+
+let bing_web_search = function (search) {
+ 
+  let request_params = {
+        method : 'GET',
+        hostname : host,
+        path : path + '?q=' + encodeURIComponent(search),
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request(request_params, response_handler);
+    req.end();
+}
+
+
+
+app.get('/search/:query',function(req,resp){
+  var query = req.params.query;
+  var offset = req.query.offset||0;
+
+  var request_params = {
+        method : 'GET',
+        hostname : host,
+        path : path + '?count=10&q=' + query+"&offset="+offset,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+  var req = https.request(request_params, function(response){
+    
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        body = JSON.parse(body);
+        k = body.value;
+      
+      var result = "";
+        for(var i=0;i<body.value.length;i++){
+          var temp = {
+            "page_url":body.value[i].hostPageUrl,
+            "alt":body.value[i].name,
+            "content_url":body.value[i].webSearchUrl
+          }  
+          result+=JSON.stringify(temp);
+          result+="\n";
+          
+        }
+      
+        resp.end(result);
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+  });
+    
+  
+  req.end();
+  
+})
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
